3a7415468222baf44c3aa14399aff76f0e93b3da
Frank Ang <franklogic8@gmail.com>
2023-01-13 20:07:28 +0800

    start wireframe for DataSetConfigEditor

--- src/components/DataSetConfigEditor.js
+++ src/components/DataSetConfigEditor.js
@@ -23,86 +23,82 @@ function DataSetConfigEditor({dataSetConfig}) {
                             justifyContent: 'left',
                         }}>Data Set Configuration Wizard</Card.Header>
                         <Card.Body>
-                            <Container fluid="true">
-                                <Form>
-                                    <Accordion className="m-2" activeKey="0">
-                                        <Accordion.Item eventKey="0">
-                                            <Accordion.Header>Prepare Source</Accordion.Header>
-                                            <Accordion.Body>
-                                                <Container fluid="true">
-                                                
-                                                    <Row className="mb-3">
-                                                        <Form.Group as={Col} className="mb-3" controlId="dataSetName">
-                                                                <Form.Label>Data Set Name</Form.Label>
-                                                                <Form.Control type="text" placeholder="Must be a unique name" />
-                                                        </Form.Group>
-                                                    </Row>
-                                                    <Row className="mb-3">
-                                                        <Form.Group as={Col} className="mb-3" controlId="dataSetSourcePath">
-                                                                <Form.Label>Source Path</Form.Label>
-                                                                <Form.Control type="text" placeholder="e.g. s3://somebucket/somepath or mounted NFS path" />
-                                                                <Button variant="outline-primary" className="float-right">Browse S3...</Button>
+                            <Form>
+                                <Accordion className="m-2">
+                                    <Accordion.Item eventKey="0">
+                                        <Accordion.Header>Prepare Source</Accordion.Header>
+                                        <Accordion.Body>
+                                            <Container fluid="true">
+                                            
+                                                <Row className="mb-3">
+                                                    <Form.Group as={Col} className="mb-3" controlId="dataSetName">
+                                                            <Form.Control type="text" placeholder="Unique Data Set Name" />
+                                                    </Form.Group>
+                                                </Row>
+                                                <Row className="mb-3">
+                                                    <Form.Group as={Col} className="mb-3" controlId="dataSetSourcePath">
+                                                            <Form.Control type="text" placeholder="Path of Source Data (S3 or NFS)" />
+                                                            <Button variant="primary" className="float-right">Select S3 path...</Button>{' '}
 
-                                                        </Form.Group>
-                                                        
-                                                    </Row>
-                                                    <Row className="mb-3">
-                                                        <Col>                                        
-                                                            Data Set Name: , Source Data type: ( S3 path, NFS path). Advanced data replication setting,.
-                                                        </Col>
-                                                    </Row>
-                                                </Container>
+                                                    </Form.Group>
+                                                    
+                                                </Row>
+                                                <Row className="mb-3">
+                                                    <Form.Group className="mb-3" controlId="advancedPrep">
+                                                        <Form.Check type="checkbox" label="Advanced Data Preparation Settings" />
+                                                    </Form.Group>
+                                                </Row>
+                                            </Container>
 
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-                                        <Accordion.Item eventKey="1">
-                                            <Accordion.Header>Data Replication Policy</Accordion.Header>
-                                            <Accordion.Body>
-                                                [ Replication Table ... ] add remove rows. [ SP Catalog search filter ]
-                                                <Accordion.Item>
-                                                    <Accordion.Header>Sub-accordion Item</Accordion.Header>
-                                                    <Accordion.Body>
-                                                    </Accordion.Body>
-                                                </Accordion.Item>
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-                                        <Accordion.Item eventKey="2">
-                                            <Accordion.Header>Configure more shit</Accordion.Header>
-                                            <Accordion.Body>
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-                                        <Accordion.Item eventKey="3">
-                                            <Accordion.Header>Confirm</Accordion.Header>
-                                            <Accordion.Body>
+                                        </Accordion.Body>
+                                    </Accordion.Item>
 
-                                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                    <Form.Check type="checkbox" label="I agree to sell my eternal soul." />
-                                                </Form.Group>
+                                    <Accordion.Item eventKey="1">
+                                        <Accordion.Header>Data Replication Policy</Accordion.Header>
+                                        <Accordion.Body>
+                                            [ Replication Table ... ] add remove rows. [ SP Catalog search filter ]
 
-                                                <Table striped="columns" bordered hover responsive className="selectableTable">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>1</th>
-                                                            <th>2</th>
-                                                            <th>3</th>
-                                                            <th>4</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <tr style={{ textAlign: "left" }}>
-                                                            <td>Foo</td>
-                                                            <td>value here</td>
-                                                            <td>value here</td>
-                                                            <td>value here</td>
-                                                        </tr>
-                                                    </tbody>
-                                                </Table>
 
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-                                    </Accordion>
-                                </Form>
-                            </Container>
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+
+                                    <Accordion.Item eventKey="2">
+                                        <Accordion.Header>Configure more shit</Accordion.Header>
+                                        <Accordion.Body>
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+
+                                    <Accordion.Item eventKey="3">
+                                        <Accordion.Header>Confirm</Accordion.Header>
+                                        <Accordion.Body>
+
+                                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                                <Form.Check type="checkbox" label="I agree to sell my eternal soul." />
+                                            </Form.Group>
+
+                                            <Table striped="columns" bordered hover responsive className="selectableTable">
+                                                <thead>
+                                                    <tr>
+                                                        <th>1</th>
+                                                        <th>2</th>
+                                                        <th>3</th>
+                                                        <th>4</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr style={{ textAlign: "left" }}>
+                                                        <td>Foo</td>
+                                                        <td>value here</td>
+                                                        <td>value here</td>
+                                                        <td>value here</td>
+                                                    </tr>
+                                                </tbody>
+                                            </Table>
+
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                </Accordion>
+                            </Form>
                         </Card.Body>
                     </Card>
                 </Row>
