c324f8854a8dada537f7fc70d0da2d4ae5edb5a1
Frank Ang <franklogic8@gmail.com>
2023-01-25 00:28:49 +0800

    add server, static responses

--- server/index.js
+++ server/index.js
@@ -1,4 +1,54 @@
-const express = require("express");
+const express = require('express')
+const app = express()
 const PORT = 8181;
-const server = express();
-server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+const {exec, execFile} = require('child_process');
+
+// Singularity 
+
+app.get('/replications', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    execFile(`/bin/cat`, ["samples/replications-list.json"], (error, stdout, stderr) => {
+        res.send(JSON.stringify(JSON.parse(stdout)));
+    })
+})
+
+app.get('/replication/:id', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    execFile(`/bin/cat`, ["samples/replication-63cfc9708795ad5b46ffb7fe.json"], (error, stdout, stderr) => {
+        res.send(JSON.stringify(JSON.parse(stdout)));
+    })
+})
+
+// Lotus
+
+app.get('/chainheight', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    execFile(`/bin/cat`, ["samples/lotus.ChainHead.json"], (error, stdout, stderr) => {
+        res.send(JSON.stringify(JSON.parse(stdout)));
+    })
+})
+
+// Retrieval
+
+app.get('/fil-ls/*', (req, res) => {
+    console.log(`fil-ls. path param: ${req.params[0]}`);
+    res.setHeader('Content-Type', 'application/json');
+    execFile(`/bin/cat`, ["samples/fil-ls.json"], (error, stdout, stderr) => {
+        res.send(JSON.stringify(JSON.parse(stdout)));
+    })
+})
+
+app.get('/fil-cp/*', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    execFile(`/bin/cat`, ["samples/fil-ls.json"], (error, stdout, stderr) => {
+        res.send(`FILE CONTENT TODO. Path: ${req.params[0]}`);
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send('')
+})
+
+app.listen(PORT, () => {
+    console.log(`Filecoin Storage Gateway Service listening on port ${PORT}`)
+})
